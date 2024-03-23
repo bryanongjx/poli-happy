5fa4e92c5a76782da897b7fde6bf054b121eaf98
bryanongjx <bryanong1589@gmail.com>
2024-03-23 19:34:54 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
+import AnimatedGif from './AnimatedGif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <AnimatedGif src = "https://media1.tenor.com/m/aKFaZBrZFYcAAAAC/excited-spin.gif" alt = "oops something went wrong" />
       </header>
     </div>
   );
