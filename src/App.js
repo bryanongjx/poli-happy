acd44060e8b476836a92425a485e148f74d42ec1
bryanongjx <bryanong1589@gmail.com>
2024-03-23 19:39:52 +0800

    add text

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <div>dis is happy poli</div>
+      <br/>
       <AnimatedGif src = "https://media1.tenor.com/m/aKFaZBrZFYcAAAAC/excited-spin.gif" alt = "oops something went wrong" />
       </header>
     </div>
